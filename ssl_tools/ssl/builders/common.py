079b78cd28d848bc2871430c06504542431ae460
Otavio Napoli <otavio.napoli@gmail.com>
2023-11-24 19:18:48 +0000

    Few changes to CPC
    Signed-off-by: Otavio Napoli <otavio.napoli@gmail.com>

--- ssl_tools/ssl/builders/common.py
+++ ssl_tools/ssl/builders/common.py
@@ -1,5 +1,30 @@
 import torch
 
+
+class Discriminator(torch.nn.Module):
+    def __init__(self, input_size: int = 10):
+        super(Discriminator, self).__init__()
+        self.input_size = input_size
+
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(2 * self.input_size, 4 * self.input_size),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Dropout(0.5),
+            torch.nn.Linear(4 * self.input_size, 1),
+        )
+
+        torch.nn.init.xavier_uniform_(self.model[0].weight)
+        torch.nn.init.xavier_uniform_(self.model[3].weight)
+
+    def forward(self, x, x_tild):
+        """
+        Predict the probability of the two inputs belonging to the same neighbourhood.
+        """
+        x_all = torch.cat([x, x_tild], -1)
+        p = self.model(x_all)
+        return p.view((-1,))
+
+
 class StateClassifier(torch.nn.Module):
     def __init__(self, input_size: int = 10, n_classes: int = 6):
         super(StateClassifier, self).__init__()
@@ -30,26 +55,4 @@ class SimpleClassifier(torch.nn.Module):
         return y
 
 
-class Discriminator(torch.nn.Module):
-    def __init__(self, input_size, device: str = "cpu"):
-        super(Discriminator, self).__init__()
-        self.input_size = input_size
-
-        self.model = torch.nn.Sequential(
-            torch.nn.Linear(2 * self.input_size, 4 * self.input_size),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Dropout(0.5),
-            torch.nn.Linear(4 * self.input_size, 1),
-        ).to(device)
-
-        torch.nn.init.xavier_uniform_(self.model[0].weight)
-        torch.nn.init.xavier_uniform_(self.model[3].weight)
-
-    def forward(self, x, x_tild):
-        """
-        Predict the probability of the two inputs belonging to the same neighbourhood.
-        """
-        x_all = torch.cat([x, x_tild], -1)
-        p = self.model(x_all)
-        return p.view((-1,))
 
