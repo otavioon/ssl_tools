55b66c2bed4bb2c5730ede9e8aaa67e0e9b03189
Otavio Napoli <otavio.napoli@gmail.com>
2023-12-06 16:52:39 +0000

    Updated TFC run scripts to use 6 channels
    Signed-off-by: Otavio Napoli <otavio.napoli@gmail.com>

--- ssl_tools/apps/har_classification/scripts/finetune_tfc_motionsense_motionsense.sh
+++ ssl_tools/apps/har_classification/scripts/finetune_tfc_motionsense_motionsense.sh
@@ -8,8 +8,10 @@ cd ..
     --batch_size 128 \
     --accelerator gpu \
     --devices 1 \
-    --load_backbone /workspaces/hiaac-m4/ssl_tools/ssl_tools/apps/har_classification/logs/TFC_pretrain/20231204.002134/checkpoints/last.ckpt \
+    --load_backbone /workspaces/hiaac-m4/ssl_tools/ssl_tools/apps/har_classification/logs/pretrain/TFC/2023-12-06_12-52-18/checkpoints/last.ckpt \
     --training_mode finetune \
     --encoding_size 128 \
-    --features_as_channels False \
-    --length_alignment 360
+    --features_as_channels True \
+    --length_alignment 60 \
+    --in_channels 6 \
+    --update_backbone False
--- ssl_tools/apps/har_classification/scripts/pretrain_tfc_motionsense.sh
+++ ssl_tools/apps/har_classification/scripts/pretrain_tfc_motionsense.sh
@@ -5,10 +5,11 @@ cd ..
 ./tfc.py \
     /workspaces/hiaac-m4/ssl_tools/data/standartized_balanced/MotionSense \
     --epochs 100 \
-    --batch_size 128 \
+    --batch_size 4 \
     --accelerator gpu \
     --devices 1  \
     --checkpoint_metric train_loss \
     --encoding_size 128 \
-    --features_as_channels False \
-    --length_alignment 360
+    --features_as_channels True \
+    --length_alignment 60 \
+    --in_channels 6 
