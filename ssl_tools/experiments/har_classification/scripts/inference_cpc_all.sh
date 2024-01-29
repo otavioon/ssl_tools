b5d0b901700d540d3ee2b8b7c9f59ecd6e78c694
Otavio Napoli <otavio.napoli@gmail.com>
2024-01-29 21:16:49 +0000

    Updated scripts

--- ssl_tools/experiments/har_classification/scripts/inference_cpc_all.sh
+++ ssl_tools/experiments/har_classification/scripts/inference_cpc_all.sh
@@ -6,7 +6,7 @@ for dset in  "KuHar" "MotionSense" "RealWorld_thigh" "RealWorld_waist" "UCI";
 do 
     ./cpc.py test \
         --data /workspaces/hiaac-m4/ssl_tools/data/standartized_balanced/${dset} \
-        --load logs/finetune/CPC/2023-12-06_19-32-19/checkpoints/last.ckpt \
+        --load logs/finetune/CPC/2024-01-29_19-58-58/checkpoints/last.ckpt \
         --batch_size 128 \
         --accelerator gpu \
         --devices 1 \
