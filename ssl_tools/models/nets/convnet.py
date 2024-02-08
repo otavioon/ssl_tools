910284c466debefa096c739d8f6855cda93c404c
Otavio Napoli <otavio.napoli@gmail.com>
2024-02-08 19:29:04 +0000

    Added MLP and 1D conv and 2D conv
    Signed-off-by: Otavio Napoli <otavio.napoli@gmail.com>

--- ssl_tools/models/nets/convnet.py
+++ ssl_tools/models/nets/convnet.py
@@ -1,31 +1,55 @@
-from typing import Dict
+from typing import Dict, Tuple
 import torch
 import lightning as L
 from torchmetrics import Accuracy
 
+from ssl_tools.models.nets.simple import SimpleClassificationNet
 
-class Simple1DConvNetwork(L.LightningModule):
-    """Model for human-activity-recognition."""
 
+class Simple1DConvNetwork(SimpleClassificationNet):
     def __init__(
         self,
-        input_channels: int = 6,
+        input_shape: Tuple[int, int] = (6, 60),
         num_classes: int = 6,
-        time_steps: int = 60,
         learning_rate: float = 1e-3,
     ):
-        super().__init__()
-        self.input_channels = input_channels
-        self.num_classes = num_classes
-        self.time_steps = time_steps
-        self.learning_rate = learning_rate
-        self.loss_func = torch.nn.CrossEntropyLoss()
-        self.metrics = {
-            "acc": Accuracy(task="multiclass", num_classes=num_classes)
-        }
+        """Create a simple 1D Convolutional Network with 3 layers and 2 fully
+        connected layers.
 
-        # Extract features, 1D conv layers
-        self.features = torch.nn.Sequential(
+        Parameters
+        ----------
+        input_shape : Tuple[int, int], optional
+            A 2-tuple containing the number of input channels and the number of
+            features, by default (6, 60).
+        num_classes : int, optional
+            Number of output classes, by default 6
+        learning_rate : float, optional
+            Learning rate for Adam optimizer, by default 1e-3
+        """
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+
+        backbone = self._create_backbone(input_channels=input_shape[0])
+        self.fc_input_channels = self._calculate_fc_input_features(
+            backbone, input_shape
+        )
+        fc = self._create_fc(self.fc_input_channels, num_classes)
+        super().__init__(
+            backbone=backbone,
+            fc=fc,
+            learning_rate=learning_rate,
+            flatten=True,
+            loss_fn=torch.nn.CrossEntropyLoss(),
+            val_metrics={
+                "acc": Accuracy(task="multiclass", num_classes=num_classes)
+            },
+            test_metrics={
+                "acc": Accuracy(task="multiclass", num_classes=num_classes)
+            },
+        )
+
+    def _create_backbone(self, input_channels: int) -> torch.nn.Module:
+        return torch.nn.Sequential(
             torch.nn.Conv1d(input_channels, 64, 5),
             torch.nn.ReLU(),
             torch.nn.Dropout(),
@@ -35,142 +59,87 @@ class Simple1DConvNetwork(L.LightningModule):
             torch.nn.Conv1d(64, 64, 5),
             torch.nn.ReLU(),
         )
-        
-        self.fc_input_features = self._calculate_fc_input_features(input_channels)
-        
-        # Classify output, fully connected layers
-        self.classifier = torch.nn.Sequential(
+
+    def _calculate_fc_input_features(
+        self, backbone: torch.nn.Module, input_shape: Tuple[int, int]
+    ) -> int:
+        """Run a single forward pass with a random input to get the number of
+        features after the convolutional layers.
+
+        Parameters
+        ----------
+        backbone : torch.nn.Module
+            The backbone of the network
+        input_shape : Tuple[int, int]
+            The input shape of the network.
+
+        Returns
+        -------
+        int
+            The number of features after the convolutional layers.
+        """
+        random_input = torch.randn(1, *input_shape)
+        with torch.no_grad():
+            out = backbone(random_input)
+        return out.view(out.size(0), -1).size(1)
+
+    def _create_fc(
+        self, input_features: int, num_classes: int
+    ) -> torch.nn.Module:
+        return torch.nn.Sequential(
             torch.nn.Dropout(),
-            torch.nn.Linear(self.fc_input_features, 128),
+            torch.nn.Linear(input_features, 128),
             torch.nn.ReLU(),
             torch.nn.Dropout(),
             torch.nn.Linear(128, num_classes),
         )
 
-    def _calculate_fc_input_features(self, input_channels):
-        # Dummy input to get the output shape after conv2
-        x = torch.randn(1, input_channels, self.time_steps)
-        with torch.no_grad():
-            out = self.features(x)
-        # Return the total number of features
-        return out.view(out.size(0), -1).size(1)
 
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), self.fc_input_features)
-        out = self.classifier(x)
-        return out
-    
-    def loss_function(self, X, y):
-        loss = self.loss_func(X, y)
-        return loss
-
-    def _common_step(self, batch, batch_idx, prefix):
-        x, y = batch
-        y_hat = self.forward(x)
-        loss = torch.nn.functional.cross_entropy(y_hat, y)
-        self.log(
-            f"{prefix}_loss",
-            loss,
-            sync_dist=True,
-            on_epoch=True,
-            on_step=False,
-            prog_bar=True,
-            logger=True,
-        )
-        return loss, y_hat, y
-
-    def _compute_metrics(
-        self, y_hat: torch.Tensor, y: torch.Tensor, stage: str
-    ) -> Dict[str, float]:
-        """Compute the metrics.
+class Simple2DConvNetwork(SimpleClassificationNet):
+    def __init__(
+        self,
+        input_shape: Tuple[int, int, int] = (6, 1, 60),
+        num_classes: int = 6,
+        learning_rate: float = 1e-3,
+    ):
+        """Create a simple 2D Convolutional Network with 3 layers and 2 fully
+        connected layers.
 
         Parameters
         ----------
-        y_hat : torch.Tensor
-            The predictions of the model
-        y : _type_
-            The ground truth labels
-        stage : str
-            The stage of the training loop (train, val or test)
-
-        Returns
-        -------
-        Dict[str, float]
-            A dictionary containing the metrics. The keys are the names of the
-            metrics, and the values are the values of the metrics.
+        input_shape : Tuple[int, int, int], optional
+            A 3-tuple containing the number of input channels, and the number of
+            the 2D input shape, by default (6, 1, 60).
+        num_classes : int, optional
+            Number of output classes, by default 6
+        learning_rate : float, optional
+            Learning rate for Adam optimizer, by default 1e-3
         """
-        return {
-            f"{stage}_{metric_name}": metric.to(self.device)(y_hat, y)
-            for metric_name, metric in self.metrics.items()
-        }
-
-    def training_step(self, batch, batch_idx):
-        loss, y_hat, y = self._common_step(batch, batch_idx, prefix="train")
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        loss, y_hat, y = self._common_step(batch, batch_idx, prefix="val")
-
-        if self.metrics is not None:
-            results = self._compute_metrics(y_hat, y, "val")
-            self.log_dict(
-                results,
-                on_step=False,
-                on_epoch=True,
-                prog_bar=True,
-                logger=True,
-            )
-
-        return loss
-
-    def test_step(self, batch, batch_idx):
-        loss, y_hat, y = self._common_step(batch, batch_idx, prefix="test")
-
-        if self.metrics is not None:
-            results = self._compute_metrics(y_hat, y, "test")
-            self.log_dict(
-                results,
-                on_step=False,
-                on_epoch=True,
-                prog_bar=True,
-                logger=True,
-            )
-
-        return loss
-
-    def predict_step(self, batch, batch_idx, dataloader_idx=None):
-        return self.forward(batch)
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(
-            self.parameters(),
-            lr=self.learning_rate,
-        )
-        return optimizer
-
-
-
-# FROM https://github.com/jindongwang/Deep-learning-activity-recognition/blob/master/pytorch/network.py
-class Simple2DConvNetwork(L.LightningModule):
-    def __init__(
-        self,
-        input_channels: int = 10,
-        num_classes: int = 6,
-        time_steps: int = 60,
-        learning_rate: float = 1e-3,
-    ):
-        super().__init__()
-        self.input_channels = input_channels
+        self.input_shape = input_shape
         self.num_classes = num_classes
-        self.time_steps = time_steps
-        self.loss_func = torch.nn.CrossEntropyLoss()
-        self.learning_rate = learning_rate
-        self.metrics = {
-            "acc": Accuracy(task="multiclass", num_classes=num_classes)
-        }
 
-        self.conv1 = torch.nn.Sequential(
+        backbone = self._create_backbone(input_channels=input_shape[0])
+        self.fc_input_channels = self._calculate_fc_input_features(
+            backbone, input_shape
+        )
+        fc = self._create_fc(self.fc_input_channels, num_classes)
+        super().__init__(
+            backbone=backbone,
+            fc=fc,
+            learning_rate=learning_rate,
+            flatten=True,
+            loss_fn=torch.nn.CrossEntropyLoss(),
+            val_metrics={
+                "acc": Accuracy(task="multiclass", num_classes=num_classes)
+            },
+            test_metrics={
+                "acc": Accuracy(task="multiclass", num_classes=num_classes)
+            },
+        )
+
+    def _create_backbone(self, input_channels: int) -> torch.nn.Module:
+        return torch.nn.Sequential(
+            # First 2D convolutional layer
             torch.nn.Conv2d(
                 in_channels=input_channels,
                 out_channels=32,
@@ -178,8 +147,7 @@ class Simple2DConvNetwork(L.LightningModule):
             ),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=(1, 2), stride=2),
-        )
-        self.conv2 = torch.nn.Sequential(
+            # Second 2D convolutional layer
             torch.nn.Conv2d(
                 in_channels=32, out_channels=64, kernel_size=(1, input_channels)
             ),
@@ -187,127 +155,94 @@ class Simple2DConvNetwork(L.LightningModule):
             torch.nn.MaxPool2d(kernel_size=(1, 2), stride=2),
         )
 
-        self.fc_input_features = self._calculate_fc_input_features(
-            input_channels
-        )
-
-        self.fc1 = torch.nn.Sequential(
-            torch.nn.Linear(
-                in_features=self.fc_input_features, out_features=1000
-            ),
-            torch.nn.ReLU(),
-        )
-        self.fc2 = torch.nn.Sequential(
-            torch.nn.Linear(in_features=1000, out_features=500), torch.nn.ReLU()
-        )
-        self.fc3 = torch.nn.Sequential(
-            torch.nn.Linear(in_features=500, out_features=num_classes)
-        )
-
-    def _calculate_fc_input_features(self, input_channels):
-        # Dummy input to get the output shape after conv2
-        x = torch.randn(1, input_channels, 1, self.time_steps)
-        with torch.no_grad():
-            out = self.conv1(x)
-            out = self.conv2(out)
-        # Return the total number of features
-        return out.view(out.size(0), -1).size(1)
-
-    def loss_function(self, X, y):
-        loss = self.loss_func(X, y)
-        return loss
-
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.conv2(out)
-        out = out.view(out.size(0), -1)  # Flatten the output for fully
-        out = self.fc1(out)
-        out = self.fc2(out)
-        out = self.fc3(out)
-        return out
-
-    def _common_step(self, batch, batch_idx, prefix):
-        x, y = batch
-        if x.ndim == 3:
-            x = x.unsqueeze(2)
-        y_hat = self.forward(x)
-        loss = torch.nn.functional.cross_entropy(y_hat, y)
-        self.log(
-            f"{prefix}_loss",
-            loss,
-            sync_dist=True,
-            on_epoch=True,
-            on_step=False,
-            prog_bar=True,
-            logger=True,
-        )
-        return loss, y_hat, y
-
-    def _compute_metrics(
-        self, y_hat: torch.Tensor, y: torch.Tensor, stage: str
-    ) -> Dict[str, float]:
-        """Compute the metrics.
+    def _calculate_fc_input_features(
+        self, backbone: torch.nn.Module, input_shape: Tuple[int, int, int]
+    ) -> int:
+        """Run a single forward pass with a random input to get the number of
+        features after the convolutional layers.
 
         Parameters
         ----------
-        y_hat : torch.Tensor
-            The predictions of the model
-        y : _type_
-            The ground truth labels
-        stage : str
-            The stage of the training loop (train, val or test)
+        backbone : torch.nn.Module
+            The backbone of the network
+        input_shape : Tuple[int, int, int]
+            The input shape of the network.
 
         Returns
         -------
-        Dict[str, float]
-            A dictionary containing the metrics. The keys are the names of the
-            metrics, and the values are the values of the metrics.
+        int
+            The number of features after the convolutional layers.
         """
-        return {
-            f"{stage}_{metric_name}": metric.to(self.device)(y_hat, y)
-            for metric_name, metric in self.metrics.items()
-        }
+        random_input = torch.randn(1, *input_shape)
+        with torch.no_grad():
+            out = backbone(random_input)
+        return out.view(out.size(0), -1).size(1)
 
-    def training_step(self, batch, batch_idx):
-        loss, y_hat, y = self._common_step(batch, batch_idx, prefix="train")
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        loss, y_hat, y = self._common_step(batch, batch_idx, prefix="val")
-
-        if self.metrics is not None:
-            results = self._compute_metrics(y_hat, y, "val")
-            self.log_dict(
-                results,
-                on_step=False,
-                on_epoch=True,
-                prog_bar=True,
-                logger=True,
-            )
-
-        return loss
-
-    def test_step(self, batch, batch_idx):
-        loss, y_hat, y = self._common_step(batch, batch_idx, prefix="test")
-
-        if self.metrics is not None:
-            results = self._compute_metrics(y_hat, y, "test")
-            self.log_dict(
-                results,
-                on_step=False,
-                on_epoch=True,
-                prog_bar=True,
-                logger=True,
-            )
-
-        return loss
-
-    def predict_step(self, batch, batch_idx, dataloader_idx=None):
-        return self.forward(batch)
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(
-            self.parameters(),
-            lr=self.learning_rate,
+    def _create_fc(
+        self, input_features: int, num_classes: int
+    ) -> torch.nn.Module:
+        return torch.nn.Sequential(
+            torch.nn.Linear(in_features=input_features, out_features=1000),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=1000, out_features=500),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=500, out_features=num_classes),
         )
-        return optimizer
+
+
+def main():
+    from torch.utils.data import DataLoader
+
+    class RandomDataset:
+        def __init__(
+            self,
+            num_samples: int = 64,
+            num_classes: int = 6,
+            input_shape: tuple = (6, 60),
+        ):
+            self.num_samples = num_samples
+            self.num_classes = num_classes
+            self.input_shape = input_shape
+
+        def __len__(self):
+            return self.num_samples
+
+        def __getitem__(self, idx):
+            return (
+                torch.randn(*self.input_shape),
+                torch.randint(0, self.num_classes, (1,)).item(),
+            )
+
+    class RandomDataModule(L.LightningDataModule):
+        def __init__(
+            self, num_samples, num_classes, input_shape, batch_size: int = 1
+        ):
+            super().__init__()
+            self.num_samples = num_samples
+            self.num_classes = num_classes
+            self.input_shape = input_shape
+            self.batch_size = batch_size
+
+        def train_dataloader(self):
+            return DataLoader(
+                RandomDataset(
+                    self.num_samples, self.num_classes, self.input_shape
+                ),
+                batch_size=self.batch_size,
+            )
+
+    data_module = RandomDataModule(
+        num_samples=10, num_classes=6, input_shape=(6, 1, 60), batch_size=8
+    )
+
+    model = Simple2DConvNetwork(input_shape=(6, 1, 60), num_classes=6)
+
+    trainer = L.Trainer(
+        max_epochs=1, logger=False, devices=1, accelerator="cpu"
+    )
+
+    trainer.fit(model, datamodule=data_module)
+
+
+if __name__ == "__main__":
+    main()
---
+++ ssl_tools/models/nets/simple.py
@@ -0,0 +1,185 @@
+from typing import Dict
+import torch
+import lightning as L
+
+
+class SimpleClassificationNet(L.LightningModule):
+    def __init__(
+        self,
+        backbone: torch.nn.Module,
+        fc: torch.nn.Module,
+        learning_rate: float = 1e-3,
+        flatten: bool = True,
+        loss_fn: torch.nn.Module = None,
+        train_metrics: Dict[str, torch.Tensor] = None,
+        val_metrics: Dict[str, torch.Tensor] = None,
+        test_metrics: Dict[str, torch.Tensor] = None,
+    ):
+        super().__init__()
+        self.backbone = backbone
+        self.fc = fc
+        self.learning_rate = learning_rate
+        self.flatten = flatten
+        self.loss_fn = loss_fn or torch.nn.CrossEntropyLoss()
+        self.metrics = {
+            "train": train_metrics or {},
+            "val": val_metrics or {},
+            "test": test_metrics or {},
+        }
+
+    def loss_func(self, y_hat, y):
+        loss = self.loss_fn(y_hat, y)
+        return loss
+
+    def forward(self, x: torch.Tensor):
+        x = self.backbone(x)
+        if self.flatten:
+            x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    def compute_metrics(self, y_hat, y, step_name):
+        for metric_name, metric_fn in self.metrics[step_name].items():
+            metric = metric_fn.to(self.device)(y_hat, y)
+            self.log(
+                f"{step_name}_{metric_name}",
+                metric,
+                on_step=False,
+                on_epoch=True,
+                prog_bar=True,
+                logger=True,
+            )
+
+    def single_step(self, batch: torch.Tensor, batch_idx: int, step_name: str):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = self.loss_func(y_hat, y)
+        self.log(
+            f"{step_name}_loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+        self.compute_metrics(y_hat, y, step_name)
+        return loss
+
+    def training_step(self, batch: torch.Tensor, batch_idx: int):
+        return self.single_step(batch, batch_idx, "train")
+
+    def validation_step(self, batch: torch.Tensor, batch_idx: int):
+        return self.single_step(batch, batch_idx, "val")
+
+    def test_step(self, batch: torch.Tensor, batch_idx: int):
+        return self.single_step(batch, batch_idx, "test")
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=None):
+        x, y = batch
+        y_hat = self.forward(x)
+        return y_hat
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(
+            self.parameters(),
+            lr=self.learning_rate,
+        )
+        return optimizer
+
+
+class MLPClassificator(SimpleClassificationNet):
+    def __init__(
+        self,
+        input_size: int,
+        hidden_size: int,
+        num_hidden_layers: int,
+        output_size: int,
+        learning_rate: float = 1e-3,
+        flatten: bool = True,
+        loss_fn: torch.nn.Module = None,
+        train_metrics: Dict[str, torch.Tensor] = None,
+        val_metrics: Dict[str, torch.Tensor] = None,
+        test_metrics: Dict[str, torch.Tensor] = None,
+    ):
+        backbone = torch.nn.Sequential()
+        for i in range(num_hidden_layers):
+            if i == 0:
+                backbone.add_module(
+                    f"fc{i+1}", torch.nn.Linear(input_size, hidden_size)
+                )
+            else:
+                backbone.add_module(
+                    f"fc{i+1}", torch.nn.Linear(hidden_size, hidden_size)
+                )
+            backbone.add_module(f"relu{i+1}", torch.nn.ReLU())
+        fc = torch.nn.Linear(hidden_size, output_size)
+        super().__init__(
+            backbone=backbone,
+            fc=fc,
+            learning_rate=learning_rate,
+            flatten=flatten,
+            loss_fn=loss_fn,
+            train_metrics=train_metrics,
+            val_metrics=val_metrics,
+            test_metrics=test_metrics,
+        )
+
+
+# def main():
+#     from torch.utils.data import DataLoader
+
+#     class RandomDataset:
+#         def __init__(
+#             self,
+#             num_samples: int = 64,
+#             num_classes: int = 6,
+#             input_shape: tuple = (6, 60),
+#         ):
+#             self.num_samples = num_samples
+#             self.num_classes = num_classes
+#             self.input_shape = input_shape
+
+#         def __len__(self):
+#             return self.num_samples
+
+#         def __getitem__(self, idx):
+#             return (
+#                 torch.randn(*self.input_shape),
+#                 torch.randint(0, self.num_classes, (1,)).item(),
+#             )
+
+#     class RandomDataModule(L.LightningDataModule):
+#         def __init__(
+#             self, num_samples, num_classes, input_shape, batch_size: int = 1
+#         ):
+#             super().__init__()
+#             self.num_samples = num_samples
+#             self.num_classes = num_classes
+#             self.input_shape = input_shape
+#             self.batch_size = batch_size
+
+#         def train_dataloader(self):
+#             return DataLoader(
+#                 RandomDataset(
+#                     self.num_samples, self.num_classes, self.input_shape
+#                 ),
+#                 batch_size=self.batch_size,
+#             )
+
+#     data_module = RandomDataModule(
+#         num_samples=10, num_classes=6, input_shape=(6 * 60,), batch_size=8
+#     )
+
+#     model = MLPClassificator(
+#         input_size=6 * 60, hidden_size=128, num_hidden_layers=2, output_size=6
+#     )
+
+#     trainer = L.Trainer(
+#         max_epochs=1, logger=False, devices=1, accelerator="gpu"
+#     )
+
+#     trainer.fit(model, datamodule=data_module)
+
+
+# if __name__ == "__main__":
+#     main()
