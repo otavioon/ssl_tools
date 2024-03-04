4b75b44322db99247a5f3a561a6e51b29888f079
Otavio Napoli <otavio.napoli@gmail.com>
2024-03-04 01:06:10 +0000

    Added run scripts

---
+++ ssl_tools/pipelines/har_classification/evaluate_all_train.sh
@@ -0,0 +1,1 @@
+python evaluator.py evaluate-all --experiment_name har_results_train --experiment_id 594152521954189262  --root_dataset_dir /workspaces/hiaac-m4/data/standartized_balanced/ --batch_size 256 --accelerator gpu --devices 1 --config_dir configs/base/test --log_dir mlruns/ --stage_to_evaluate "train" --skip_existing true
